23d0543684c10fcc86d4517d4deacf470f596758
AndreyOOP <andrew_yudaichev@i.ua>
2017-11-16 00:00:05 +0200

    Eagle Solution

--- src/Exercices/DynamicPrograms/Eagle/E_3.java
+++ src/Exercices/DynamicPrograms/Eagle/E_3.java
@@ -2,24 +2,15 @@ package Exercices.DynamicPrograms.Eagle;
 
 import java.util.Scanner;
 
-public class E_2 {
+public class E_3 {
 
-    static int n, k, x = 0;
+    static int n, k;
     static Scanner sc = new Scanner(System.in);
     static int[][] cache = new int[1001][1001];
 
     public static void main(String[] args) {
 
-        for(int i=1; i<=1000; i++) //k = 1
-            cache[i][1] = i;
-
         cache[2][2] = 2; //k = 2
-
-        for (int k=2; k<=1000; k++) {
-            for(int i=1; i<=1000; i++){
-                f(i, k);
-            }
-        }
 
         while (true){
 
@@ -28,8 +19,6 @@ public class E_2 {
             if(n == 0 && k == 0) return;
 
             System.out.println( f(n, k));
-//            System.out.println( x);
-            x = 0;
         }
     }
 
@@ -37,28 +26,27 @@ public class E_2 {
 
         int temp, answ = 1001;
 
-        //if(k>100) return cache[n][100];
         if(cache[n][k] != 0) return cache[n][k];
 
-        if(k > 11){
+        if(k > 10){ //10 is lower limit, e.g in case of infinite qty of balls we can decrease to n/2 on each ball - 1024 => 10
 
-            int mn = 1001;
+            int mn = 1001, t;
 
-            for(int i=1; i<=11; i++){
-                int t = f(n, i);
+            for(int i=1; i<=10; i++){
+                t = f(n, i);
                 mn = Math.min(mn, t);
             }
             return mn;
         }
 
+        if(k == 1) return n;
         if(k > n ) return f(n, n);
         if(k == n) return f(n, n-1);
 
-        for(int i=2; i<n; i++){ //from 2 should be...
+        for(int i=2; i<n; i++){
 
-            temp = Math.max( f(i-1, k-1), f(n-i, k));
+            temp = Math.max(f(i-1, k-1), f(n-i, k));
             answ = Math.min(answ, temp);
-            x++;
         }
 
         cache[n][k] = answ + 1;
